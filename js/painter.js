be58d7eca8b83d5335fc0f63400ff0d79480ec42
Katur7 <grimurk@gmail.com>
2015-01-27 13:25:21 +0000

    The mutterfnk DrawEllipse function again

--- js/painter.js
+++ js/painter.js
@@ -82,7 +82,26 @@ $(document).ready(function(){
         //TODO
     }
 
+    // DrawEllipse function gotten from http://stackoverflow.com/questions/2172798/how-to-draw-an-oval-in-html5-canvas/2173084#2173084 through link in slides
+    function drawEllipse(ctx, x, y, w, h, lw, c) {
+        var kappa = 0.5522848,
+            ox = (w / 2) * kappa, // control point offset horizontal
+            oy = (h / 2) * kappa, // control point offset vertical
+            xe = x + w,           // x-end
+            ye = y + h,           // y-end
+            xm = x + w / 2,       // x-middle
+            ym = y + h / 2;       // y-middle
 
+        ctx.beginPath();
+        ctx.moveTo(x, ym);
+        ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+        ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+        ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+        ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+        ctx.strokeStyle = c;
+        ctx.lineWidth = lw;
+        ctx.stroke();
+    }
 
     // Mouse handlers
     $("#tempPainter").mousedown(function(e) {
